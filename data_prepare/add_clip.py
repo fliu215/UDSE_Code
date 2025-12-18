16da2b2dbf050560290d59e33fa120efe235841a
fliu215 <96053781+fliu215@users.noreply.github.com>
2025-12-18 16:48:55 +0800

    Add files via upload

---
+++ data_prepare/add_clip.py
@@ -0,0 +1,45 @@
+import numpy as np
+import librosa
+import soundfile as sf
+from rich.progress import track
+import argparse
+import os
+
+def clipping(speech_sample, min_quantile: float = 0.0, max_quantile: float = 0.9):
+    """Apply the clipping distortion to the input signal.
+
+    Args:
+        speech_sample (np.ndarray): a single speech sample (1, Time)
+        min_quantile (float): lower bound on the quantile of samples to be clipped
+        max_quantile (float): upper bound on the quantile of samples to be clipped
+
+    Returns:
+        ret (np.ndarray): clipped speech sample (1, Time)
+    """
+    q = np.array([min_quantile, max_quantile])
+    min_, max_ = np.quantile(speech_sample, q, axis=-1, keepdims=False)
+    # per-channel clipping
+    ret = np.stack(
+        [
+            np.clip(speech_sample[i], min_[i], max_[i])
+            for i in range(speech_sample.shape[0])
+        ],
+        axis=0,
+    )
+    return ret
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clean_wav_dir', default='/train/aiyang/data/voice_bank/trainset_reverb_noisy_48k')
+    parser.add_argument('--output_dir', default='/train/aiyang/data/voice_bank/clip_trainset_rnclip_48k')
+    args = parser.parse_args()
+
+    input_lists = os.listdir(args.clean_wav_dir)
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for index in track(input_lists):
+        clean_signal, sr = librosa.load(os.path.join(args.clean_wav_dir, index), sr=48000)
+        clean_signal = clean_signal[None,:]
+        clipped_signal = clipping(clean_signal, min_quantile=0.1, max_quantile=0.9)
+        sf.write(os.path.join(args.output_dir, index), clipped_signal[0], sr)
---
+++ data_prepare/add_reverb_noise.py
@@ -0,0 +1,55 @@
+import librosa
+import sys
+import os 
+import glob
+import random
+from scipy import signal
+import numpy as np
+import soundfile as sf
+from rich.progress import track
+
+def get_all_audio_files(root_dir, exts=(".wav", ".flac")):
+    audio_files = []
+    for ext in exts:
+        audio_files.extend(glob.glob(os.path.join(root_dir, "**", f"*{ext}"), recursive=True))
+    return sorted(audio_files)
+
+wav_dir = 'data/Medley-solos-DB/train_16k'
+rir_dir = 'data/dataset_reverb_16k/RIR_files'
+noise_dir = '/train/aiyang/DEMAND-16k/train'
+output = 'data/Medley-solos-DB/n_train_16k'
+os.makedirs(output, exist_ok=True)
+wav_index = get_all_audio_files(wav_dir)
+rir_index = os.listdir(rir_dir)
+noise_index = get_all_audio_files(noise_dir)
+
+
+for index in track(wav_index):
+    clean_wav, sr = librosa.load(index, sr=None)
+    # add reverb
+    clean_power = np.sum(clean_wav ** 2) / len(clean_wav)
+    rir = random.choice(rir_index)
+    rir_wav, _ = librosa.load(os.path.join(rir_dir, rir), sr=None)
+    reverb_wav = signal.convolve(clean_wav, rir_wav, mode="full")[:len(clean_wav)]
+    reverb_power = np.sum(reverb_wav ** 2) / len(reverb_wav)
+    reverb_wav = np.sqrt(clean_power / max(reverb_power, 1e-10)) * reverb_wav
+    # reverb_wav = clean_wav
+
+    # add noise
+    noise = random.choice(noise_index)
+    noise_audio, _ = librosa.load(noise, sr=None)
+    snr = random.uniform(0,5)
+    if len(noise_audio) < len(reverb_wav):
+        repeat_times = int(np.ceil(len(reverb_wav) / len(noise_audio)))
+        noise_audio = np.tile(noise_audio, repeat_times)
+    rand_start = random.randrange(0, len(noise_audio) - len(reverb_wav) + 1)
+    noise_audio = noise_audio[rand_start: rand_start + len(reverb_wav)]
+    reverb_power = np.sum(reverb_wav ** 2) / len(reverb_wav)
+    noise_power = np.sum(noise_audio ** 2) / len(noise_audio)
+    scale = (np.sqrt(reverb_power) / np.sqrt(max(noise_power, 1e-10))) * (10 ** (-snr / 20))
+    noisy_audio = reverb_wav + scale * noise_audio
+
+    name = os.path.basename(index)
+    out_rel = os.path.splitext(name)[0] + '.wav'
+
+    sf.write(os.path.join(output, out_rel), noisy_audio, sr, subtype='PCM_16')
